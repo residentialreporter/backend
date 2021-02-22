31d98a1b910fce6873cc5b637d9ef5a0d9abcf55
ri0t <riot@c-base.org>
2021-02-22 16:42:15 +0100

    adds logos/icons and extends icon generator

--- dev/iconfoo.py
+++ dev/iconfoo.py
@@ -7,6 +7,16 @@ from copy import copy
 
 input_image = sys.argv[1]
 output_folder = sys.argv[2]
+platform = sys.argv[3]
+
+android_sizes = {
+    'hdpi': 72,
+    'ldpi': 32,
+    'mdpi': 48,
+    'xhdpi': 96,
+    'xxhdpi': 144,
+    'xxxhdpi': 192
+}
 
 icon = Image.open(input_image).convert("RGBA")
 
@@ -17,34 +27,49 @@ for _, _, files in os.walk("./"):
         item = str(item)
         print(item)
 
-        prefix = "icon-"
-        try:
-            _, rest = item.split(prefix, 1)
-        except ValueError:
-            _, rest = item.split(prefix.rstrip("-"), 1)
-
-        size, _ = rest.split(".png")
-
-        if "@" in size:
-            pixels, zoom = size.split("@")
-
-            if pixels == 'small':
-                pixels = 29
-            elif pixels == '':
-                pixels = 57
-            else:
-                pixels = float(pixels)
-
-            zoom = int(zoom.rstrip("x"))
-        else:
+        if platform == 'ios':
+            prefix = "icon-"
             try:
-                pixels = float(size)
+                _, rest = item.split(prefix, 1)
             except ValueError:
-                pixels = 29
-            zoom = 1
+                _, rest = item.split(prefix.rstrip("-"), 1)
 
-        pixels = int(pixels * zoom)
-        print(pixels)
+            size, _ = rest.split(".png")
+
+            if "@" in size:
+                pixels, zoom = size.split("@")
+
+                if pixels == 'small':
+                    pixels = 29
+                elif pixels == '':
+                    pixels = 57
+                else:
+                    pixels = float(pixels)
+
+                zoom = int(zoom.rstrip("x"))
+            else:
+                try:
+                    pixels = float(size)
+                except ValueError:
+                    pixels = 29
+                zoom = 1
+
+            pixels = int(pixels * zoom)
+            print(pixels)
+        elif platform == 'android':
+            prefix = "drawable-"
+
+            try:
+                _, rest = item.split(prefix, 1)
+            except ValueError as e:
+                print("Value error:", e)
+                continue
+
+            size, _ = rest.split("-icon.png")
+
+            pixels = android_sizes[size]
+
+            print(pixels)
 
         resized_icon = copy(icon)
 
